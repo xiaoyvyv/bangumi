475233954ee729dea40744dadc84a320218b5124
why <wanghuaiyu@highlight.mobi>
2024-01-01 19:10:36 +0800

    [Feat]: v1.0.3(5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.xiaoyv.bangumi"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
