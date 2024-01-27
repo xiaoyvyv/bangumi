f5fac0bcd60d596bc1b337deb091907977521f7c
why <wanghuaiyu@highlight.mobi>
2024-01-28 00:53:28 +0800

    [Feat]: abiFilters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,9 +21,9 @@ android {
 
         ndk {
             abiFilters.add("arm64-v8a")
-//            abiFilters.add("armeabi-v7a")
-//            abiFilters.add("x86")
-//            abiFilters.add("x86_64")
+            abiFilters.add("armeabi-v7a")
+            abiFilters.add("x86")
+            abiFilters.add("x86_64")
         }
     }
 
