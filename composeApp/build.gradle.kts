b68318a43e69c2bab83763ead5029e03ac9b3e5c
why <wanghuaiyv@gmail.com>
2026-08-29 16:58:09 +0800

    ios: disable DevirtualizationAnalysis in release builds

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
 
 val appVersionName = providers.gradleProperty("appVersionName").get()
 val desktopPackageVersion = appVersionName
@@ -22,6 +23,12 @@ kotlin {
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
+        iosTarget.binaries.configureEach {
+            if (buildType == NativeBuildType.RELEASE) {
+                freeCompilerArgs += "-Xdisable-phases=DevirtualizationAnalysis"
+            }
+        }
+
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
