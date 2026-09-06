f4f84870f83b946b78d7439d28d833413f06c466
why <wanghuaiyv@gmail.com>
2026-09-06 13:17:59 +0800

    fix: collect index issue

--- shared/data/src/commonMain/kotlin/com/xiaoyv/bangumi/shared/data/api/client/plugin/JsonContentTypePlugin.kt
+++ shared/data/src/commonMain/kotlin/com/xiaoyv/bangumi/shared/data/api/client/plugin/JsonContentTypePlugin.kt
@@ -7,7 +7,7 @@ import io.ktor.http.HttpMethod
 import io.ktor.util.appendIfNameAbsent
 
 /**
- * 自动为 POST/PUT/PATCH 请求按需添加 application/json Content-Type
+ * 自动为 POST/PATCH 请求按需添加 application/json Content-Type
  * 避免影响没有 Body 的 DELETE/GET 请求引发 Fastify 415 报错
  *
  * 插件只在调用方尚未指定 Content-Type 时补充请求头，因此表单、文件上传等显式声明的内容类型
@@ -15,7 +15,7 @@ import io.ktor.util.appendIfNameAbsent
  */
 val JsonContentTypePlugin = createClientPlugin("JsonContentTypePlugin") {
     onRequest { request, _ ->
-        if (request.method == HttpMethod.Post || request.method == HttpMethod.Put || request.method == HttpMethod.Patch) {
+        if (request.method == HttpMethod.Post || request.method == HttpMethod.Patch) {
             request.headers.appendIfNameAbsent(HttpHeaders.ContentType, ContentType.Application.Json.toString())
         }
     }
